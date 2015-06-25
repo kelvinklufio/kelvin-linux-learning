bbd83488554031f3f24bf646c05645c6994562b0
Kelvin Klufio <omanyemensah@gmail.com>
2015-06-25 21:42:23 +0000

    added comment to the hello.sh script.

--- hello/hello.sh
+++ hello/hello.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
+#The hello.sh script print to the screen "Hello Word"
+#
+#Author: Kelvin Omanye Mensah Klufio
+#Email: <omanyemensah@gmail.com>
+#Date created: 25 June 2015
+#Version: 1.0
 
 echo "Hello World!."
