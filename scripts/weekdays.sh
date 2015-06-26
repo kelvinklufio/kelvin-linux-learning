0c19a4c5b49031103fcb1cd41b4f40d9505c5678
Kelvin Klufio <omanyemensah@gmail.com>
2015-06-26 14:02:10 +0000

    added weekdays. closes #5

---
+++ scripts/weekdays.sh
@@ -0,0 +1,17 @@
+#!/bin/bash 
+#This programm called weekdays print to the screen the day of the week
+
+#Author: Kelvin Omanye Mensah Klufio
+#Email: <omanyemensah@gmail.com>
+#Mobile: +233(0)206965535,(0)245230113
+#Date created: 26th June 2015
+
+echo Monday 
+echo Tuesday
+echo Wednesday
+echo Thursday
+echo Friday
+echo Saturday
+echo Sunday
+echo
+echo "These are the day of the week in order"
